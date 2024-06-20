87324f9fd0b7aa3aeceb558f9b55c6073f64f8de
Deepak Sharma <112716576+uddisharma@users.noreply.github.com>
2024-06-20 16:27:21 +0530

    Add files via upload

---
+++ webRTC/backend/dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const express_1 = __importDefault(require("express"));
+const socket_io_1 = require("socket.io");
+const UserManger_1 = require("./managers/UserManger");
+const node_cron_1 = __importDefault(require("node-cron"));
+node_cron_1.default.schedule('* * * * *', () => {
+    console.log('Running a task every minute');
+});
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.send("healthy server");
+});
+const server = http_1.default.createServer(app); // Use 'app' instead of 'http'
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+const userManager = new UserManger_1.UserManager();
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    userManager.addUser("randomName", socket);
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+        userManager.removeUser(socket.id);
+    });
+});
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ webRTC/frontend/src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+
+
+
+interface Window {
+    pcr: RTCPeerConnection | null; // Adjust the type based on what 'pc' is
+}
