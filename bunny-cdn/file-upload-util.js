3db382b0bf443535568f5aa8b783d21f9d826c82
Deepak Sharma <112716576+uddisharma@users.noreply.github.com>
2024-07-02 15:24:13 +0530

    Add files via upload

---
+++ bunny-cdn/file-upload-util.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import fs from "fs";
+
+export const handleFileUpload = async (file) => {
+  const fileStream = fs.createReadStream(file.path);
+  const uniqueFilename = `${Date.now()}-${file.filename}-${file.originalname}`;
+
+  let yourStorageZone = "thribute1";
+  const response = await axios.put(
+    `https://sg.storage.bunnycdn.com/${yourStorageZone}/${uniqueFilename}`,
+    fileStream,
+    {
+      headers: {
+        AccessKey: "3f1864e3-abbd-4657-86fbe9081e47-4042-4dc1",
+      },
+    }
+  )
+
+  if (response.data) {
+    return `https://thribute.b-cdn.net/${uniqueFilename}`;
+  } else {
+    return false;
+  }
+};
